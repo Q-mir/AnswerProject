c62eb2e66e1dbd6703641705624ab6862b258048
Вячеслав Сквороцов <swank123@yandex.ru>
2025-06-11 19:09:16 +0700

    ##

---
+++ Assets/Game/Config/QuizElementCongif.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
+namespace Assets.Game.Scripts
+{
+	[CreateAssetMenu(fileName = "QuizElementConfig", menuName = "MySo/Quiz/QuizElementCongif")]
+	public class QuizElementCongif : ScriptableObject
+	{
+        [SerializeField] public string QuestionText { get; private set; }
+        [SerializeField] public Sprite QuestionImage{ get; private set; }
+        [SerializeField] public string[] Answers{ get; private set; }
+    }
+}
